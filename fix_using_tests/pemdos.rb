c9a658bd05a6462e18dde5490c2b174a00967e53
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-25 20:53:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] = "s"
-   puts "s" * 10 + string
+  if string[0] == "s"
+   p "s" * 10 + string
   else
    string
   end
