a4b7503ff8519b40e4342721ec1e4db5eae89f21
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-25 20:31:53 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,13 +1,9 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-  p  10.times {"s"} && string
-    
+  if string[0] = "s"
+    10 * "s" + string
   else
     string
   end
 end
-
-snake_it_up("Saran")
