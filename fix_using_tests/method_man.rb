614d847238426c80cf8648f95952be0f197d4f00
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-25 19:31:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,9 +2,9 @@
 
 
 def start_game(player1, player2)
-    puts "Hello #{player1} & #{player2}"
+  p "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1)
-  "#{player1} is better than #{player2}"
+  p "#{player1} is better than #{player2}"
 end
