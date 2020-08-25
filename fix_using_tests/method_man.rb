7a02c33e2e32b0b21398537114609aa7ca8e80c7
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-08-25 19:23:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,6 +1,5 @@
 # don't forget to add: require 'pry'
 
-require 'pry'
 
 def start_game(player1, player2)
     puts "Hello #{player1} & #{player2}"
@@ -8,6 +7,5 @@ def start_game(player1, player2)
 end
 
 def play_game(player2)
-  binding.pry
   "#{player1} is better than #{player2}"
 end
